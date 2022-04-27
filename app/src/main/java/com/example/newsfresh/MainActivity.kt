ece71ec9f4a62a2360de2fa00e3e9380066a98d4
Adwiti <adwitibishwanath@gmail.com>
2022-04-27 23:23:37 +0530

    Completed News Fresh App and Tested its working.

--- app/src/main/java/com/example/newsfresh/MainActivity.kt
+++ app/src/main/java/com/example/newsfresh/MainActivity.kt
@@ -1,28 +1,65 @@
 package com.example.newsfresh
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 import com.example.newsfresh.databinding.ActivityMainBinding
 
 
 private lateinit var mainBinding: ActivityMainBinding
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), NewsItemClicked {
+    private lateinit var mAdapter: NewsListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
-        var RecyclerView= mainBinding.recyclerView
-        RecyclerView.layoutManager=LinearLayoutManager(this)
-        val items=fetchData()
-        val adapter:NewsListAdapter= NewsListAdapter(items)
-        RecyclerView.adapter=adapter
+        var recyclerView = mainBinding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        fetchData()
+        mAdapter = NewsListAdapter(this)
+        recyclerView.adapter = mAdapter
     }
-    private fun fetchData():ArrayList<String>{
-        val list=ArrayList<String>()
-        for(i in 0 until 1000){
-            list.add("Items $i")
-        }
-    return list
+
+    private fun fetchData() {
+        val url =
+            "https://saurav.tech/NewsAPI/top-headlines/category/technology/in.json"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            {
+                val newsJsonArray = it.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for (i in 0 until newsJsonArray.length()) {
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+
+                }
+                mAdapter.updateNews(newsArray)
+
+            },
+            {
+                it -> Log.d("Main Activity", it.toString())
+            }
+
+        )
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+
+    }
+
+    override fun onItemClicked(item: News) {
+        val builder = CustomTabsIntent.Builder();
+        val customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(this, Uri.parse(item.url));
     }
 }
